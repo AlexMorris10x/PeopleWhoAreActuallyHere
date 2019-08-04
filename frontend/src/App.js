b90f83371d0bc7f12cd54b68f003ff14b8a1bfba
Alex Morris <alexmorris10x@gmail.com>
2019-08-03 17:11:32 -0700

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+<form>
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
+  );
+}
+
+export default App;
