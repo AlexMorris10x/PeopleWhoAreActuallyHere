ce1c3ff3810d7a4e082331a1bffc92acc3c021c9
Jay Querie <jay@querie.cc>
2019-08-29 23:37:32 -0700

    repackage qr-web to isolate it from other package paths

--- packages/qr-web/bin/build.sh
+++ packages/qr-web/bin/build.sh
@@ -1,13 +1,27 @@
-#!/usr/bin/env sh
+#!/usr/bin/env dash
 
-SCRIPT_PATH=`dirname "$0"`; SCRIPT_PATH=`eval "cd \"$SCRIPT_PATH\" && pwd"`
+SCRIPT_PATH=`dirname "$0"`;
+SCRIPT_PATH=`eval "cd \"$SCRIPT_PATH\" && pwd"`
 
-rm -rf build;
-mkdir -p build;
+if [ -z "${BUILD_DIR}" ]; then
+    echo "missing environment variable BUILD_DIR" >&2
+    exit 1
+fi
+
+# install npm modules if necessary
+if [ -z "${SCRIPT_PATH}/../node_modules" ]; then
+    (cd "${SCRIPT_PATH}/.."; npm install)
+fi
+
+PATH=$SCRIPT_PATH/../node_modules/.bin:$PATH
+
+QR_BUILD_DIR="${BUILD_DIR}/qr-web"
+rm -rf "${QR_BUILD_DIR}"
+mkdir -p "${QR_BUILD_DIR}"
 
 (
-    cd build;
+    cd "${QR_BUILD_DIR}"
     cp -r $SCRIPT_PATH/../src/* .
 
-    $SCRIPT_PATH/../node_modules/.bin/browserify $SCRIPT_PATH/../../shared-js/TokenGen.js > ./lib/TokenGen.js
+    browserify $SCRIPT_PATH/../src/index.js > ./lib/index.js
 )
